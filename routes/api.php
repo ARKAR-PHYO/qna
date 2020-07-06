ec6357803703090bdeda90ab2985187f4033673b
ARKAR-PHYO <arkarphyo.edu@gmail.com>
2020-07-06 19:33:05 +0630

    made some changes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Http\Request;
+// use Psr\Http\Message\ServerRequestInterface;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +14,8 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('/token', 'Auth\LoginController@getToken');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
